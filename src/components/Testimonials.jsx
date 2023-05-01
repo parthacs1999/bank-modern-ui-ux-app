f8745ca253ccdd65300c7068f603339a21803700
Partha Chakraborty <partha.c1999@gmail.com>
2023-05-01 18:56:22 +0530

    minor error in testimonials corrected

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { feedback } from '../constants';
 import styles from '../style';
-import FeedbackCard from './FeedBackCard';
+import FeedbackCard from './FeedbackCard';
 const Testimonials = () => {
   return (
     <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
