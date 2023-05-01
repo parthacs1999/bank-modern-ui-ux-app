67987e1c0cd8ad2b2c2fb4c104b519839d53638e
Partha Chakraborty <partha.c1999@gmail.com>
2023-05-01 17:51:41 +0530

    Testimonials section created

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card <br className='sm:block hidden' />deal in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque nesciunt ratione nobis. In a quod voluptate ipsa magnam eaque quis?
+        </p>
+        <Button styles={"mt-10"} />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='w-[100%] h-[100%]' />
+      </div>
+
+    </section>
   )
 }
 
-export default CardDeal
+export default CardDeal;
